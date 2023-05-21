dd17d2878823833f04c5c23149bbbcfb48823ae2
SergeyPurusov <purusovsergey@mail.ru>
2023-05-22 00:17:56 +0500

    Commit

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,7 @@
 package koschei.config;
 
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
+//    public static Duck5 getDuck5(Egg6 egg6) {
+//        return new Duck5(egg6);
+//    }
+
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
 }
